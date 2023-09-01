f2e129b786ed0767b9a1862120103a667549f6f2
francois lalay <francois.lalay@gmail.com>
2023-09-01 10:46:10 +0200

    add delete projects route

--- src/Entity/Projects.php
+++ src/Entity/Projects.php
@@ -6,6 +6,7 @@ use DateTimeImmutable;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\Put;
 use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Delete;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\ProjectsRepository;
@@ -24,6 +25,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
 #[GetCollection]
 #[Post]
 #[Put]
+#[Delete]
 class Projects
 {
     #[ORM\Id]
